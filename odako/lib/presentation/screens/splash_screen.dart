0cf4418dd4a1da44629d168df010e6798ca70718
Hayrunnisa sarı <151072139+hyrs049@users.noreply.github.com>
2025-07-19 20:20:09 +0300

    splash screen second

--- odako/lib/presentation/screens/splash_screen.dart
+++ odako/lib/presentation/screens/splash_screen.dart
@@ -19,7 +19,7 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Future<void> _handleStartupRouting() async {
-    await Future.delayed(const Duration(seconds: 1));
+    await Future.delayed(const Duration(seconds: 3));
     final user = FirebaseAuth.instance.currentUser;
     if (!mounted) return;
     if (user == null) {
